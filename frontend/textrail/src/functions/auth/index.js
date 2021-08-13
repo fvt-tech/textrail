54be9a7c68fb5dc937f280c1e15d489925426863
MuniruIssah <imuniru@st.ug.edu.gh>
2021-08-13 11:58:14 +0000

    auth functions update

--- frontend/textrail/src/functions/auth/index.js
+++ frontend/textrail/src/functions/auth/index.js
@@ -1,17 +1,18 @@
 import axios from "axios";
-const API_AUTH_BASE_URL = "http://localhost:8000/auth/";
+import { API_AUTH_URL } from "../../constants";
 
 //Textrail Login
 export const textrailLogin = async (user) => {
-  const result = await axios.post(`${API_AUTH_BASE_URL}login`, user);
-  return result;
+  const response = await axios.post(`${API_AUTH_URL}/login`, user);
+  console.log(response.data)
+  return response;
 };
 // Textrail SignUp
 export const textrailSignup = async (user) => {
-  const result = await axios.post(`${API_AUTH_BASE_URL}signup`, user);
-  return result;
+  const response = await axios.post(`${API_AUTH_URL}/signup`, user);
+  return response;
 };
 // Textrail Logout
 export const textrailLogout = async () => {
-  await axios.get(`${API_AUTH_BASE_URL}logout`);
+  await axios.get(`${API_AUTH_URL}/logout`);
 };
