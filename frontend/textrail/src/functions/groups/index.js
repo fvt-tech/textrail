e575e3d7e3ce79adc29df622aeb3763a75c877b4
Muniru Issah <imuniru@st.ug.edu.gh>
2021-08-15 11:49:00 +0000

    Groups:Updated the functions for the groups

--- frontend/textrail/src/functions/groups/index.js
+++ frontend/textrail/src/functions/groups/index.js
@@ -2,25 +2,27 @@ import axios from "axios";
 import { API_BASE_URL } from "../../constants";
 
 //Get all groups
-export const textrailGetGroups = async () => {
-  const response = await axios.post(`${API_BASE_URL}getGroups`, null, {
+export const textrailGetGroups = async (user) => {
+  const response = await axios.post(`${API_BASE_URL}getGroups`, {user}, {
     withCredentials: true,
   });
-  console.log(response.data);
+  return response.data;
 };
 //Get a group
-export const textrailGetGroup = async () => {
-  const response = await axios.post(`${API_BASE_URL}getGroup`, null, {
+export const textrailGetGroup = async (user,id) => {
+  const response = await axios.post(`${API_BASE_URL}getGroup`, {user,id}, {
     withCredentials: true,
   });
   console.log(response.data);
+  return response;
 };
 //Add a group
-export const textrailAddGroup = async () => {
-  const response = await axios.post(`${API_BASE_URL}getGroups`, null, {
+export const textrailAddGroup = async (group) => {
+  const response = await axios.post(`${API_BASE_URL}addGroup`, group, {
     withCredentials: true,
   });
   console.log(response.data);
+  return response;
 };
 //Edit a group
 export const textrailEditGroup = async () => {
@@ -36,3 +38,8 @@ export const textrailDeleteGroup = async () => {
   });
   console.log(response.data);
 };
+
+//Add contacts to a group
+export const textrailAddContactsToGroup=()=>{
+
+}
