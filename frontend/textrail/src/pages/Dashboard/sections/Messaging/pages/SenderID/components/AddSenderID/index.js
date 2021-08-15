9f56932f54cd517e0ce7434318b092fa219b694e
Muniru Issah <imuniru@st.ug.edu.gh>
2021-08-15 15:36:12 +0000

    SenderID:integrating the ui with the functions

--- frontend/textrail/src/pages/Dashboard/sections/Messaging/pages/SenderID/components/AddSenderID/index.js
+++ frontend/textrail/src/pages/Dashboard/sections/Messaging/pages/SenderID/components/AddSenderID/index.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Input } from "antd";
-const AddSenderID = () => {
+import { textrailAddSenderID } from "../../../../../../../../functions/senderID";
+const AddSenderID = ({ user }) => {
+  const [senderId, setSenderId] = useState("");
+  //Handler for changing senderId field
+  const handleSenderIdChange = (e) => setSenderId(e.target.value);
   //Handle Adding a sender id
-  const handleAddSenderID = () => {};
+  const handleAddSenderID = async () => {
+    console.log(senderId);
+    const response = await textrailAddSenderID(senderId, user);
+    console.log(response);
+  };
   return (
     <div>
       <span
@@ -15,12 +23,22 @@ const AddSenderID = () => {
       >
         Create a new Sender ID
       </span>
-      <Form layout="inline" style={{ marginBottom: "3rem" }}>
+      <Form
+        layout="inline"
+        style={{ marginBottom: "3rem" }}
+        onFinish={handleAddSenderID}
+      >
         <Form.Item label="Sender ID">
-          <Input placeholder="Enter sender ID" />
+          <Input
+            placeholder="Enter sender ID"
+            value={senderId}
+            onChange={handleSenderIdChange}
+          />
         </Form.Item>
         <Form.Item>
-          <Button type="primary">Create</Button>
+          <Button type="primary" htmlType="submit">
+            Create
+          </Button>
         </Form.Item>
       </Form>
     </div>
--- frontend/textrail/src/pages/Dashboard/sections/Messaging/pages/SenderID/index.js
+++ frontend/textrail/src/pages/Dashboard/sections/Messaging/pages/SenderID/index.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Empty } from "antd";
 import DashboardCard from "../../../../../../components/DashboardCard";
 import SenderIDList from "./components/SenderIDList";
 import AddSenderID from "./components/AddSenderID";
-const list = [
-  {
-    senderId: "Issah",
-  },
-  {
-    senderId: "Kwaku",
-  },
-];
+import { textrailGetSenderIDs } from "../../../../../../functions/senderID";
+
+
 const SenderIDs = () => {
+  const [senderIds, setSenderIds] = useState([])
+  const [user,setUser]=useState()
+  //Get all sender ids
+  useEffect(() => {
+    const getSenderIds = async () => {
+      const { data: user } = JSON.parse(localStorage.getItem("user"));
+      setUser(user)
+      const response = await textrailGetSenderIDs(user._id);
+      console.log(response);
+      // setSenderIds(response.data)
+    };
+    getSenderIds();
+  }, []);
   return (
     <div className="dashboardPage">
       <h1>Sender IDs</h1>
       <DashboardCard>
-        <AddSenderID/>
-        <SenderIDList list={list} />
+        <AddSenderID user={user} />
+        <SenderIDList list={senderIds} />
       </DashboardCard>
     </div>
   );
