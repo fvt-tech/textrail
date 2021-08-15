05ebb0abde05271224e8c6001b5e5d1845ad76f6
Muniru Issah <imuniru@st.ug.edu.gh>
2021-08-15 14:00:44 +0000

    dded the sender id page

--- frontend/textrail/src/pages/Dashboard/sections/Messaging/index.js
+++ frontend/textrail/src/pages/Dashboard/sections/Messaging/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import AddressBook from "./pages/AddressBook";
 import ScheduledMessages from "./pages/ScheduledMessages";
+import SenderIDs from "./pages/SenderID";
 import SendSMS from "./pages/SendSMS";
 import SmsTemplates from "./pages/SmsTemplates";
 const MessagingSection = (props) => {
   return (
     <Switch>
+      <Route path={`${props.route}/senderIDs`} component={SenderIDs} />
       <Route path={`${props.route}/addressBook`} component={AddressBook} />
       <Route path={`${props.route}/templates`} component={SmsTemplates} />
       <Route path={`${props.route}/scheduled`} component={ScheduledMessages} />
